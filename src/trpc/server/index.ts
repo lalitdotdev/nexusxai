d290a46f90b5ee8ccf212c9d6cf2463866b55a8d
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-15 12:08:52 +0530

    feat: create TRPC context with database, session, and headers; initialize TRPC router with custom context type.

--- src/trpc/server/index.ts
+++ src/trpc/server/index.ts
@@ -1,14 +1,30 @@
+import { TRPCError, initTRPC } from '@trpc/server'
+
 // import { AIService } from '@/ai/ai.service'
 // import { Role } from '@/util/types'
 import { auth } from '@clerk/nextjs/server'
 // import { authorizeUser } from './util'
 import db from '@/lib/db'
-import { initTRPC } from '@trpc/server'
-
-export const t = initTRPC.create() // Create a new instance of TRPC
 
 // Todo:  create a TRPC context that includes the database connection, session, and any other services you want to use in your procedures
-// / Create a new instance of TRPC
+
+export const createTRPCContext = async (opts: { headers: Headers }) => {
+  const session = auth()
+
+  /**
+   * TODO: Add  other services you want to use in your procedures here (add ai service)
+   */
+
+  return {
+    db: db,
+    session,
+
+    ...opts,
+  }
+}
+
+// create a TRPC router with the context you created above
+const t = initTRPC.context<typeof createTRPCContext>().create()
 
 export const createTRPCRouter = t.router
 export const publicProcedure = t.procedure
