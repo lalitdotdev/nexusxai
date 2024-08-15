6385a44e45a04d72ddafee56834c8530bf5e0c6f
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-15 12:10:24 +0530

    feat: define appRouter with users query procedure using publicProcedure; set up initial router structure.

--- src/trpc/server/routers/index.ts
+++ src/trpc/server/routers/index.ts
@@ -1,5 +1,18 @@
 import { createTRPCRouter, publicProcedure } from '..'
 
-export const appRouter = createTRPCRouter({})
+export const appRouter = createTRPCRouter({
+  // Define your procedures here
+  // publicProcedure,
+
+  users: publicProcedure.query(({ ctx }) => {
+    return ctx.db.user.findMany()
+  }),
+
+  // protectedProcedure,
+  // adminProcedure,
+  // userProcedure,
+  // aiProcedure,
+  // etc.
+})
 
 export type AppRouter = typeof appRouter
