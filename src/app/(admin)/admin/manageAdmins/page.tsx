9698ed7529ec0e7e324d450081d665f9f5872945
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-18 17:07:03 +0530

    ✅ feat(admin management page): Admin management page with CreateAdmin form and delete functionality.

---
+++ src/app/(admin)/admin/manageAdmins/page.tsx
@@ -0,0 +1,31 @@
+'use client '
+
+import { CreateAdmin } from '@/components/globalTemplates/CreateAdmin'
+import { DeleteAdminButton } from '@/components/organisms/DeleteAdminButton'
+import { Title2 } from '@/components/atoms/typography'
+import { UserCard } from '@/components/organisms/UserCard'
+import { trpcServer } from '@/trpc/clients/server'
+
+export default async function ManageAdminsPage() {
+  const admins = await trpcServer.admins.findAll.query()
+
+  return (
+    <div className="flex flex-col gap-4 px-6 py-4">
+      <Title2 className="mb-2">Manage Admins</Title2>
+
+      <hr />
+      <CreateAdmin />
+
+      <Title2 className="mt-8 mb-2">Admins</Title2>
+      <div className="space-y-3">
+        {admins.map((admin) => (
+          <UserCard key={admin.User.id} user={admin.User}>
+            <div className="flex justify-end mt-2">
+              <DeleteAdminButton id={admin.id} />
+            </div>
+          </UserCard>
+        ))}
+      </div>
+    </div>
+  )
+}
