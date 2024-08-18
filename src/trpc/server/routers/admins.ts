ade1f622bfcf54fcfea668e0bc0ee0417423a520
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-18 15:59:11 +0530

    🤠 feat(admin): implement TRPC route to retrieve admin profile with associated user details - Added 'adminMe' query to 'adminRoutes' using protectedProcedure.

---
+++ src/trpc/server/routers/admins.ts
@@ -0,0 +1,13 @@
+import { createTRPCRouter, protectedProcedure, publicProcedure } from '..'
+
+import { TRPCError } from '@trpc/server'
+import { schemaCreateUser } from '@/forms/CreateUser'
+
+export const adminRoutes = createTRPCRouter({
+  adminMe: protectedProcedure().query(({ ctx }) => {
+    return ctx.db.admin.findUnique({
+      where: { id: ctx.userId },
+      include: { User: true },
+    })
+  }),
+})
