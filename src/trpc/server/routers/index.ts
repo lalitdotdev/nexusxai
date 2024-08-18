523a05457e54342b57dd21d6f4db135e0c9f4769
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-18 16:13:17 +0530

    🚏 feat(trpc server routers): set up tRPC server routes with integrated admin routes and a public procedure for fetching users.

--- src/trpc/server/routers/index.ts
+++ src/trpc/server/routers/index.ts
@@ -1,13 +1,18 @@
 import { createTRPCRouter, publicProcedure } from '..'
 
+import { adminRoutes } from './admins'
+import { articlesRoutes } from './articles'
+import { reporterRoutes } from './reporters'
+
 export const appRouter = createTRPCRouter({
+  admins: adminRoutes,
+  reporters: reporterRoutes,
+  articles: articlesRoutes,
   // Define your procedures here
   // publicProcedure,
-
   users: publicProcedure.query(({ ctx }) => {
     return ctx.db.user.findMany()
   }),
-
   // protectedProcedure,
   // adminProcedure,
   // userProcedure,
