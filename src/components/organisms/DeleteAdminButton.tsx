51b8ac1de00d8e897c98931910275c254b9791f3
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-18 16:57:22 +0530

    ❌ feat(delete admin ): add DeleteAdminButton component with TRPC mutation and toast notifications for successful admin deletion.

---
+++ src/components/organisms/DeleteAdminButton.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Button } from '../atoms/button'
+import { CreateUserInput } from '@/forms/CreateUser'
+import { revalidatePath } from '@/utils/actions/revalidatePath'
+import { toast } from 'sonner'
+import { trpcClient } from '@/trpc/clients/client'
+
+export const DeleteAdminButton = ({ id }: CreateUserInput) => {
+    const {
+        data,
+        isLoading,
+        mutateAsync: deleteAdmin,
+    } = trpcClient.admins.delete.useMutation({
+        onSuccess() {
+            toast.success(`Admin deleted successfully.`, {
+                description: 'Request assistance from our admins and provide your ID.',
+
+            })
+
+        },
+    })
+
+    return (
+        <Button
+            loading={isLoading}
+            onClick={async () => {
+                await deleteAdmin({ id })
+            }}
+            variant={'destructive'}
+            size={'sm'}
+            className=""
+        >
+            Delete
+        </Button>
+    )
+}
