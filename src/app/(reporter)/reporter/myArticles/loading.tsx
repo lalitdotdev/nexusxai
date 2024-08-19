f747b11fa1c2dbb23bcef1f0694280cd5e9d8cfd
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-19 12:00:30 +0530

    🚧 chore: Add Loading component with PostSkeleton to improve UX during data fetching.

---
+++ src/app/(reporter)/reporter/myArticles/loading.tsx
@@ -0,0 +1,11 @@
+import { PostSkeleton } from "./_components/PostSkeleton";
+
+export default function Loading() {
+    // You can add any UI inside Loading, including a Skeleton.
+    return (
+        <>
+            <PostSkeleton />
+            <PostSkeleton />
+        </>
+    )
+}
