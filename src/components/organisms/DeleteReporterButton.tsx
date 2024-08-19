851566b697d67e2100bd39dc7a65a6f6c2edb0bb
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-19 11:57:30 +0530

    🔨 feat: Add DeleteReporterButton component for admin privileges, handling reporter deletion with toast notification and revalidation of the /admin/manageReporters path.

---
+++ src/components/organisms/DeleteReporterButton.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { Button } from '../atoms/button'
+import { revalidatePath } from '@/utils/actions/revalidatePath'
+import { trpcClient } from '@/trpc/clients/client'
+import { useToast } from '../molecules/Toaster/use-toast'
+
+export const DeleteReporterButton = ({ id }: CreateUserInput) => {
+  const { toast } = useToast()
+
+  const {
+    data,
+    isLoading,
+    mutateAsync: deleteReporter,
+  } = trpcClient.reporters.delete.useMutation({
+    onSuccess() {
+      toast({ title: 'Reporter deleted.' })
+      revalidatePath('/admin/manageReporters')
+    },
+  })
+
+  return (
+    <Button
+      loading={isLoading}
+      onClick={async () => {
+        await deleteReporter({ id })
+      }}
+      variant={'link'}
+      size={'none'}
+    >
+      Delete
+    </Button>
+  )
+}
