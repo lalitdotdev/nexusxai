6bc1b4999cc86d17196c29093ac1290b72cee591
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-19 12:26:47 +0530

    🧠 feat: Integrate AIService class with Pinecone and Voyage AI for user embedding creation and storage in the Pinecone index.

---
+++ src/ai/ai.service.ts
@@ -0,0 +1,70 @@
+import { Index, Pinecone, RecordMetadata } from '@pinecone-database/pinecone'
+
+import { INDEX_NAME } from './constants'
+import { RouterOutputs } from '@/trpc/clients/types'
+import { TRPCError } from '@trpc/server'
+
+export class AIService {
+  private readonly pineconeIndex: Index<RecordMetadata>
+
+  constructor() {
+    // Create a Pinecone index if it doesn't exist
+    const pinecone = new Pinecone({
+      apiKey: process.env.PINECONE_API_KEY || '',
+    })
+
+    this.pineconeIndex = pinecone.Index(INDEX_NAME)
+  }
+
+  // addUser adds a user to the Pinecone index
+  async addUser({ uid }: { uid: string }) {
+    const values = await this.createEmbedding('default user')
+    await this.pineconeIndex.upsert([
+      {
+        id: uid,
+        values: values.data[0].embedding,
+        metadata: {
+          id: uid,
+          type: 'user',
+        },
+      },
+    ])
+  }
+
+  // createEmbedding creates an embedding for a given content string using the Voyage AI API and returns the embedding values
+  private createEmbedding(content: string) {
+    const apiUrl = `https://api.voyageai.com/v1/embeddings`
+    const data = {
+      input: content,
+      model: 'voyage-2',
+    }
+
+    const voyageAIKey = process.env.VOYAGE_AI_KEY || ''
+    if (!voyageAIKey) {
+      throw new TRPCError({
+        code: 'BAD_REQUEST',
+        message: 'VOYAGE_AI_KEY is not set',
+      })
+    }
+
+    return fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${voyageAIKey}`,
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((embeddings) => {
+        return embeddings
+      })
+      .catch((err) => {
+        console.error('Error creating embedding:', err)
+        throw new TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'Error creating embedding',
+        })
+      })
+  }
+}
