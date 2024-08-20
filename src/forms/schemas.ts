44913457ea0802a90f6d5b5b0fb34a355ded999e
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-20 18:10:35 +0530

    🔧 Defined Zod schemas for article creation and update processes, including title, body, tags, and publication status.

---
+++ src/forms/schemas.ts
@@ -0,0 +1,28 @@
+import { FeedbackType } from '@prisma/client'
+import { z } from 'zod'
+
+export const schemaCreateArticle = z.object({
+  title: z.string(),
+  body: z.string(),
+  published: z.boolean(),
+  tags: z.array(z.string()),
+})
+
+export const schemaUpdateArticle = z.object({
+  articleId: z.number(),
+  published: z.boolean(),
+})
+
+export const schemaGiveFeedback = z.object({
+  articleId: z.number(),
+  type: z.nativeEnum(FeedbackType),
+})
+
+export const schemaNumberID = z.object({
+  id: z.number(),
+})
+
+export const schemaPayment = z.object({
+  userId: z.string(),
+  creditsCount: z.coerce.number(),
+})
