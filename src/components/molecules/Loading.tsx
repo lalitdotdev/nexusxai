3c23d22f347e13b8309bd943871a4de8ad1f507a
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-21 18:54:00 +0530

    feat: add customizable Loading component with ThreeDots spinner / skeleton and optional children.

--- src/components/molecules/Loading.tsx
+++ src/components/molecules/Loading.tsx
@@ -7,23 +7,23 @@ import React from 'react'
 import { cn } from '@/utils/styles'
 
 export const Loading = ({ children, className, ...props }: BaseComponent) => {
-  return (
-    <div className={cn('flex flex-col items-center my-12', className)}>
-      {children ? (
-        <div className="text-gray-600 text-xs mb-1">{children}</div>
-      ) : null}
-      <div className="-mt-10">
-        <ThreeDots
-          visible={true}
-          height="80"
-          width="80"
-          color="#6561E1"
-          radius="9"
-          ariaLabel="three-dots-loading"
-          wrapperStyle={{}}
-          wrapperClass=""
-        />
-      </div>
-    </div>
-  )
+    return (
+        <div className={cn('flex flex-col items-center my-12', className)}>
+            {children ? (
+                <div className="text-gray-600 text-xs mb-1">{children}</div>
+            ) : null}
+            <div className="-mt-10">
+                <ThreeDots
+                    visible={true}
+                    height="80"
+                    width="80"
+                    color="#6561E1"
+                    radius="9"
+                    ariaLabel="three-dots-loading"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                />
+            </div>
+        </div>
+    )
 }
