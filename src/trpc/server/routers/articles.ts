7751fce0b07d3db98c53900fdc144194c5d38af1
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-21 09:46:02 +0530

    🔒 Add Protected Article Creation for Admins and Reporters: Implemented create mutation to create articles with summary generation and AI upsert, secured by admin and reporter roles.

--- src/trpc/server/routers/articles.ts
+++ src/trpc/server/routers/articles.ts
@@ -1,8 +1,25 @@
 import { createTRPCRouter, protectedProcedure } from '..'
 
 import { fetchAndScoreRelatedArticles } from './shared/articles'
+import { schemaCreateArticle } from '@/forms/schemas'
 
 export const articlesRoutes = createTRPCRouter({
+  create: protectedProcedure('admin', 'reporter')
+    .input(schemaCreateArticle)
+    .mutation(async ({ ctx, input }) => {
+      const summaryLength = 200
+      const summary = input.body.substring(0, summaryLength)
+
+      const article = await ctx.db.article.create({
+        data: { ...input, summary, Reporter: { connect: { id: ctx.userId } } },
+      })
+      await ctx.ai.upsertArticle({
+        ...article,
+        createdAt: article.createdAt.toISOString(),
+        updatedAt: article.updatedAt.toISOString(),
+      })
+      return article
+    }),
   //   adding another user recommendations to the article object -----> protected Routes
 
   userRecommendations: protectedProcedure().query(async ({ ctx }) => {
