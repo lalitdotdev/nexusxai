b4450bbf15c1ab834e1b7979e3b512b40100da56
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-22 17:21:57 +0530

    feat: implement ArticlePage component with full article details, reactions, and related content.

---
+++ src/components/globalTemplates/ArticlePage.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { AlertBox } from '../molecules/AlertBox'
+import { DisplayDate } from '../molecules/DisplayDate'
+import Image from 'next/image'
+import { Loading } from '../molecules/Loading'
+import { MoreLikeThis } from '../organisms/MoreLikeThis'
+import { ReactionPanel } from '../organisms/ReactionPanel'
+import { ReporterInfo } from '../molecules/ReporterInfo'
+import { Separator } from '../ui/separator'
+import { Slider } from "@/components/ui/slider"
+import { cn } from '@/utils/styles'
+import { trpcClient } from '@/trpc/clients/client'
+
+export interface IArticlePageProps {
+    articleId: number
+}
+
+export const ArticlePage = ({ articleId }: IArticlePageProps) => {
+    const { data: article, isLoading } = trpcClient.articles.findOne.useQuery({
+        id: articleId,
+    })
+
+    if (isLoading) {
+        return <Loading />
+    }
+    if (!article) {
+        return <AlertBox>Article not found.</AlertBox>
+    }
+
+    return (
+        <div className={cn(' mx-auto mb-24 mt-12 ')}>
+            <h1 className={cn('text-2xl font-semibold mb-2')}>{article.title}</h1>
+            <DisplayDate dateString={article.createdAt} />
+            <ReporterInfo
+                image={article.Reporter.User.image}
+                name={article.Reporter.User.name}
+            />
+
+            <div className="mt-4 whitespace-pre-wrap text-lg ">
+
+                {article.body}
+
+            </div>
+            <ReactionPanel articleId={article.id} />
+            <Separator className="" />
+            <Slider defaultValue={[33]} max={100} step={1} />
+
+            <MoreLikeThis className="mt-8" id={article.id} />
+        </div>
+    )
+}
