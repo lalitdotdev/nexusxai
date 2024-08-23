588cc534a9e2d1d8995bfd81cb7cb4e48156dd84
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-23 10:38:24 +0530

    💳 feat(trpc app Router Index): Add Stripe and credit balance routes to app router for payment processing and user credit management.

--- src/trpc/server/routers/index.ts
+++ src/trpc/server/routers/index.ts
@@ -2,14 +2,19 @@ import { createTRPCRouter, publicProcedure } from '..'
 
 import { adminRoutes } from './admins'
 import { articlesRoutes } from './articles'
+import { creditBalanceRoutes } from './userCredits'
 import { feedbacksRoutes } from './feedbacks'
 import { reporterRoutes } from './reporters'
+import { stripeRoutes } from './stripe'
 
 export const appRouter = createTRPCRouter({
   admins: adminRoutes,
   reporters: reporterRoutes,
   articles: articlesRoutes,
   feedbacks: feedbacksRoutes,
+  creditBalance: creditBalanceRoutes,
+  stripe: stripeRoutes,
+
   // Define your procedures here
   // publicProcedure,
   users: publicProcedure.query(({ ctx }) => {
