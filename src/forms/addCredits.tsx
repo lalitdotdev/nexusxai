f4dbcfa530561bf8da677628eba4507fc95c57d3
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-23 10:35:58 +0530

    📰 feat(add credits form  hook and schema): implement form schema and hook for adding credits with default values and Zod validation.

---
+++ src/forms/addCredits.tsx
@@ -0,0 +1,20 @@
+import { schemaPayment } from './schemas'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+export const schemaAddCredits = schemaPayment.omit({ userId: true })
+
+
+// It takes a Zod schema as its generic parameter and infers the TypeScript type that matches the shape of data the schema validates.
+
+
+export type FormTypeAddCredits = z.infer<typeof schemaAddCredits>
+
+
+// exporting the form management hook
+export const useFormAddCredits = () =>
+    useForm<FormTypeAddCredits>({
+        resolver: zodResolver(schemaAddCredits),
+        defaultValues: { creditsCount: 20 },
+    })
