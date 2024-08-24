b492cde24b0e275e07a214b2f2597697939d3968
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-24 18:33:22 +0530

    feat(credit page): Add CreditsPage component to display credit balance using CreditBalance.

---
+++ src/app/(user)/user/credits/page.tsx
@@ -0,0 +1,12 @@
+import { CreditBalance } from '@/components/globalTemplates/CreditBalance'
+import React from 'react'
+
+const CreditsPage = () => {
+    return (
+        <main>
+            <CreditBalance />
+        </main>
+    )
+}
+
+export default CreditsPage
