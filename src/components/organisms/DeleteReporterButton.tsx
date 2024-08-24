ddd9d307a226ab1a6776e837abbd930a24a5b8bb
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-24 18:52:18 +0530

    🚀 Add DeleteReporterButton component for deleting reporters and revalidating path after success

--- src/components/organisms/DeleteReporterButton.tsx
+++ src/components/organisms/DeleteReporterButton.tsx
@@ -1,34 +1,35 @@
 'use client'
 
 import { Button } from '../atoms/button'
+import { CreateUserInput } from '@/forms/CreateUser'
 import { revalidatePath } from '@/utils/actions/revalidatePath'
 import { trpcClient } from '@/trpc/clients/client'
 import { useToast } from '../molecules/Toaster/use-toast'
 
 export const DeleteReporterButton = ({ id }: CreateUserInput) => {
-  const { toast } = useToast()
+    const { toast } = useToast()
 
-  const {
-    data,
-    isLoading,
-    mutateAsync: deleteReporter,
-  } = trpcClient.reporters.delete.useMutation({
-    onSuccess() {
-      toast({ title: 'Reporter deleted.' })
-      revalidatePath('/admin/manageReporters')
-    },
-  })
+    const {
+        data,
+        isLoading,
+        mutateAsync: deleteReporter,
+    } = trpcClient.reporters.delete.useMutation({
+        onSuccess() {
+            toast({ title: 'Reporter deleted.' })
+            revalidatePath('/admin/manageReporters')
+        },
+    })
 
-  return (
-    <Button
-      loading={isLoading}
-      onClick={async () => {
-        await deleteReporter({ id })
-      }}
-      variant={'link'}
-      size={'none'}
-    >
-      Delete
-    </Button>
-  )
+    return (
+        <Button
+            loading={isLoading}
+            onClick={async () => {
+                await deleteReporter({ id })
+            }}
+            variant={'link'}
+            size={'none'}
+        >
+            Delete
+        </Button>
+    )
 }
