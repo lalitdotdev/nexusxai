ae94cd6c2d0e5f96ea881ce97fdab3396ce02999
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-24 18:07:44 +0530

    📱💻 Add responsive SelectEditors component to layout for mobile and desktop views.

---
+++ src/app/(article)/article/[id]/layout.tsx
@@ -0,0 +1,23 @@
+import { SelectEditors } from '@/components/organisms/SelectEditors'
+
+export default async function Layout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div>
+            {children}
+
+            {/* For larger screens */}
+            <div className="hidden md:block fixed right-2 top-1/2 -translate-y-1/2">
+                <SelectEditors />
+            </div>
+
+            {/* For smaller screens */}
+            <div className="md:hidden fixed bottom-0 left-0 right-0">
+                <SelectEditors />
+            </div>
+        </div>
+    )
+}
