70ac87d885b153297d87cddd2485cad6aaab3092
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-24 17:33:20 +0530

    🔄 Implement DeleteEditor component for removing editors. Includes a confirmation dialog with a delete action. Utilizes TRPC mutation for editor deletion and toast notifications for success and error handling

---
+++ src/components/organisms/DeleteEditor.tsx
@@ -0,0 +1,62 @@
+'use client'
+
+import { useEffect, useState } from 'react'
+
+import { Button } from '../atoms/button'
+import { RouterOutputs } from '@/trpc/clients/types'
+import { SimpleDialog } from '../molecules/SimpleDialog'
+import { Trash } from 'lucide-react'
+import { trpcClient } from '@/trpc/clients/client'
+import { useToast } from '../molecules/Toaster/use-toast'
+
+export const DeleteEditor = ({
+    editor,
+}: {
+    editor: NonNullable<RouterOutputs['editors']['myEditors'][0]>
+}) => {
+    const utils = trpcClient.useUtils()
+    const { error, mutateAsync: deleteEditor } =
+        trpcClient.editors.delete.useMutation({
+            onSuccess: (data) => {
+                utils.editors.myEditors.invalidate()
+                toast({ title: `Editor ${data.name} updated.` })
+                setOpenDialog(false)
+            },
+        })
+    const [openDialog, setOpenDialog] = useState(false)
+    const { toast } = useToast()
+
+    useEffect(() => {
+        if (error) {
+            toast({ title: 'Action failed.' })
+        }
+    }, [error, toast])
+
+    return (
+        <div className="font-sans">
+            <Button
+                size="none"
+                variant={'ghost'}
+                onClick={() => {
+                    setOpenDialog(true)
+                }}
+            >
+                <Trash />
+            </Button>
+            <SimpleDialog
+                open={openDialog}
+                setOpen={setOpenDialog}
+                title="Delete Editor"
+            >
+                <Button
+                    onClick={async () => {
+                        await deleteEditor({ id: editor.id })
+                    }}
+                    variant={'destructive'}
+                >
+                    Delete Editor {editor.name}?
+                </Button>
+            </SimpleDialog>
+        </div>
+    )
+}
