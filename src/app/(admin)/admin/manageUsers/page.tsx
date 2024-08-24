10d53cc0718250aeec2177300c55fc2ed3947c0f
Lalit sharma <geekymonkcp742k@gmail.com>
2024-08-24 18:11:53 +0530

    feat(manage Users): ✨ Add Manage Users page with UserCard component for user management.

---
+++ src/app/(admin)/admin/manageUsers/page.tsx
@@ -0,0 +1,20 @@
+import { Title2 } from '@/components/atoms/typography'
+import { UserCard } from '@/components/organisms/UserCard'
+import { trpcServer } from '@/trpc/clients/server'
+
+export default async function ManageArticles() {
+    const users = await trpcServer.users.findAll.query()
+
+    return (
+        <main>
+            <Title2>Manage Users</Title2>
+            <div className="flex flex-col gap-4">
+                {users.map((user) => (
+                    <div key={user.id}>
+                        <UserCard user={user} />
+                    </div>
+                ))}
+            </div>
+        </main>
+    )
+}
